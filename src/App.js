c3eb33c841d793fa31faf2d1c45f6d75394eb2fb
skyorbit0 <skyorbit@126.com>
2018-05-15 11:24:40 +0800

    :sparkles: add search

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import React, {Component} from 'react';
 import './App.css';
-// Google UI
-import darkbaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import RaiseButton from 'material-ui/RaisedButton';
-import AppBar from 'material-ui/AppBar';
-import {cyan500} from 'material-ui/styles/colors';
 
 // 模拟数据
 const list = [
@@ -28,15 +21,10 @@ const list = [
     },
 ];
 
-// 自定义样式
-const muiTheme = getMuiTheme({
-    palette: {
-        textColor: cyan500,
-    },
-    appBar: {
-        height: 50,
-    },
-})
+
+// 高阶函数 传递给过滤器一个函数，该函数需要返回一个boolean值
+const isSearched = searchTerm => item =>
+    item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
 class App extends Component {
     constructor(props) {
@@ -45,13 +33,18 @@ class App extends Component {
         this.state = {
             // 属性名与变量名一致的时候可以简写
             list,
+            searchTerm: '',
         };
 
         // 绑定到类方法
+        // 使用bind()是为了将this绑定到类实例，，类方法不会自动绑定this到实例上。会无法调用state的。
         this.onDismiss = this.onDismiss.bind(this);
+        this.onSearchChange = this.onSearchChange.bind(this);
     }
 
+    // 方法初始化简写形式
     onDismiss(id) {
+        console.log(this);
         // 结果为false
         const isNotId = item => item.objectID !== id;
         // filter函数遍历数字，传入函数，如果判断是true则保留, 最后返回结果数组，并不会改变原数组
@@ -59,14 +52,23 @@ class App extends Component {
         this.setState({list: updatedList});
     }
 
+    onSearchChange(event) {
+        // 这里可以访问到事件对象
+        this.setState({
+            searchTerm: event.target.value
+        })
+    }
+
+
     render() {
         return (
-            <MuiThemeProvider muiTheme={muiTheme}>
-                <AppBar title="My AppBar"/>
-                <RaiseButton label="Dismiss"/>
+            <div className="app">
+                <form>
+                    <input type="text" onChange={this.onSearchChange}/>
+                </form>
 
                 {/*这里箭头函数使用了简洁体*/}
-                {this.state.list.map(item =>
+                {this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
                     <div key={item.objectID}>
                         <span>
                             <a href={item.url}>{item.title}</a>
@@ -74,13 +76,24 @@ class App extends Component {
                         <span>{item.author}</span>
                         <span>{item.num_comments}</span>
                         <span>{item.points}</span>
-                        {/*删除按钮*/}
+                        {/*删除按钮
+                           这里的传入事件的匿名函数被称为高阶函数
+                           倘若直接向onclick传入一个函数名，则无法向该函数传入参数
+                           如果直接向onclick传入一个带有参数的函数，那么该函数会在页面加载后立即执行
+                           因此向onclick传入一个匿名函数
+                           这里涉及到性能问题，每次执行render的时候会实例化一个高阶函数
+                        */}
                         <span>
-                            <button onClick={() => this.onDismiss(item.objectID)} type="button">Dismiss</button>
+                            <button
+                                onClick={() => this.onDismiss(item.objectID)}
+                                type="button"
+                            >
+                                Dismiss
+                            </button>
                         </span>
                     </div>
                 )}
-            </MuiThemeProvider>
+            </div>
         );
     }
 }
