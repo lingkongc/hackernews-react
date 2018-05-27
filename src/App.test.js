cee5030df25cf8ce6e206078ca35902ecb327092
skyorbit0 <skyorbit@126.com>
2018-05-27 18:34:19 +0800

    :white_check_mark: Adding Enzyme test

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 import App from './App';
 import Search from './Search';
 import Button from './Button';
 import Table from './Table';
+
+Enzyme.configure({adapter: new Adapter()});
 
 describe('App', () => {
     it('renders without crashing', () => {
@@ -73,4 +78,13 @@ describe('Table', () => {
         let tree = component.toJSON();
         expect(tree).toMatchSnapshot();
     });
+
+    it('shows two items in list', () => {
+        const element = shallow(
+            <Table {...props}/>
+        );
+        expect(element.find('.table-row').length).toBe(2);
+    });
 });
+
+
