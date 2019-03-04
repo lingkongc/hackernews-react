2de91abe9e3bbc8c8a75956af42cc64d431c6a96
skyorbit0 <skyorbit@126.com>
2019-03-04 11:57:54 +0800

    :tada:  1.0 first commit

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
 
 // 模块热替换
-if(module.hot){
+if (module.hot) {
     module.hot.accept();
 }
