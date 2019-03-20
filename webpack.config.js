b9310e21a898704a1716da9edbbb74be9c266399
skyorbit0 <skyorbit@126.com>
2019-03-20 17:32:30 +0800

    :beer: change dependencies & start a new branch

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    mode: 'development',
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    },
+    devtool: "cheap-eval-source-map",
+    entry: './src/index.js',
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babnel-loader",
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', {
+                                "targets": {
+                                    "browsers": [">1%", "not ie<=8"]
+                                }
+                            }],
+                            ['@babel/preset-react']
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: '热模块替换'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ]
+}
